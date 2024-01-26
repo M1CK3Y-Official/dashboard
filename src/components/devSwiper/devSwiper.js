8b8d38b978f88d503f7173473cee1095a9f2d209
Mike Boldsen <mikeboldsen2004@gmail.com>
2024-01-26 12:20:58 +0100

    f

--- src/components/devSwiper/devSwiper.js
+++ src/components/devSwiper/devSwiper.js
@@ -2,25 +2,63 @@
 import { register } from 'swiper/element/bundle';
 import styles from './devSwiper.module.css';
 import { Swiper, SwiperSlide } from 'swiper/react';
-// import "swiper/css";
+import { FaChevronLeft, FaChevronRight  } from "react-icons/fa";
+import "swiper/css";
 
 import Image from 'next/image';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
+
 
 const DevSwiper = ({author}) => {
 
+
+    const swiperRef = useRef(null);
+
+    // Register swiper
     useEffect(() => {
         console.log('swiper registered')
         register();
+
+        console.log(swiperRef.current.swiper)
+
     }, []);
 
+
+    // Listen for events
+    useEffect(() => {
+        swiperRef.current.addEventListener('swiperSlideChange', (e) => {
+            const [swiper] = e.detail;
+            console.log('slide changed', swiper.activeIndex);
+        });
+    }, []);
+
+    const slideNext = () => {
+        swiperRef.current.swiper.slideNext(500);
+    }
+    const slidePrev = () => {
+        swiperRef.current.swiper.slidePrev(500);
+    }
+
+
+ 
+
+    
+
+    
+    
   
 
     return (
         <div className={styles.slider}>
-            <swiper-container  pagination="true"  autoplay-delay="5000" rewind="true" effect="coverflow" navigation="true" loop="true" slides-per-view="1" keyboard="true">
+
+            <div className={styles.arrowContainer}>
+                <FaChevronLeft className={styles.arrow} onClick={() => slidePrev()}></FaChevronLeft>
+                <FaChevronRight className={styles.arrow} onClick={() => slideNext()}></FaChevronRight>
+            </div>
+
+            <swiper-container style={{ height: '100%' }}  pagination="true"  autoplay-delay="5000" rewind="true" effect="coverflow"  loop="true" slides-per-view="auto" keyboard="true" speed="500" ref={swiperRef}>
                 {author.map( (image) => {
-                    return <swiper-slide key={image._id} className={styles.slide}>
+                    return <swiper-slide key={image._id} className={styles.slide}> 
                         <Image className={styles.image} src={`/galleries${image.path}`} alt={image.name} width={image.width} height={image.height} />
                     </swiper-slide>
                 
